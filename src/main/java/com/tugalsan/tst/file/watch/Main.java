f98976846f841fddca86c018ab37171a473f628e
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-03-10 01:07:41 +0300

    .

--- src/main/java/com/tugalsan/tst/file/watch/Main.java
+++ src/main/java/com/tugalsan/tst/file/watch/Main.java
@@ -2,9 +2,12 @@ package com.tugalsan.tst.file.watch;
 
 import com.tugalsan.api.file.server.*;
 import com.tugalsan.api.log.server.*;
-import com.tugalsan.api.thread.server.async.TS_ThreadAsyncAwait;
-import com.tugalsan.api.thread.server.TS_ThreadWait;
+import com.tugalsan.api.thread.server.async.await.TS_ThreadAsyncAwait;
 import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
+import com.tugalsan.api.thread.server.sync.TS_ThreadSyncWait;
+import java.io.IOException;
+import static java.lang.System.out;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 
@@ -15,11 +18,13 @@ public class Main {
     //cd  C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.file.watch
     //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.file.watch-1.0-SNAPSHOT-jar-with-dependencies.jar
     public static void main(String... s) {
-        TS_ThreadSyncTrigger killTrigger = TS_ThreadSyncTrigger.of();
+        TS_ThreadSyncTrigger killTrigger = TS_ThreadSyncTrigger.of("main");
+        if (true) {
+            return;
+        }
         var file = Path.of("C:\\dat\\sql\\rql\\TS_LibRqlBufferUtils_autosqlweb2.json");
 
         d.cr("main", "isLocked", TS_FileUtils.isFileLocked(file));
-
         if (true) {
             return;
         }
@@ -27,8 +32,8 @@ public class Main {
         TS_FileWatchUtils.file(killTrigger, file, () -> {
             d.cr("main", "create detected", file);
         }, 60, TS_FileWatchUtils.Triggers.CREATE, TS_FileWatchUtils.Triggers.MODIFY);
-        TS_ThreadAsyncAwait.runUntil(killTrigger, Duration.ofMinutes(1), kt -> {
-            TS_ThreadWait.seconds(d.className, killTrigger, 10);
+        TS_ThreadAsyncAwait.runUntil(killTrigger.newChild("test"), Duration.ofMinutes(1), kt -> {
+            TS_ThreadSyncWait.seconds(d.className, killTrigger, 10);
             d.cr("main", "alive");
         });
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.tugalsan.tst.file.watch {
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.file;
     requires com.tugalsan.api.time;
+    requires com.tugalsan.api.union;
     requires com.tugalsan.api.function;
     requires com.tugalsan.api.thread;
 }
